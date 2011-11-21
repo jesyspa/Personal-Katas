bc34ed08059abafd47d9def6d4fde9b160335fba
Anton Golov <kdesevis@gmail.com>
2011-11-21 19:56:43 +0100

    Made tests work.

--- C++/FizzBuzzTest.cpp
+++ C++/FizzBuzzTest.cpp
@@ -1,11 +1,9 @@
-#include <gtest/gtest.h>
+#include "FizzBuzz.hpp"
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE FizzBuzzTest
+#include <boost/test/unit_test.hpp>
 
-TEST(FizzBuzz, NAME) {
-
-}
-
-int main(int argc, char* argv[]) {
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+BOOST_AUTO_TEST_CASE(NAME) {
+	
 }
 
