8e136344b1d3bc3d42467f1555fb4c602a0da016
Anton Golov <kdesevis@gmail.com>
2011-12-09 00:23:51 +0100

    Created kata and added Python base.

---
+++ Python/binary_search.py
@@ -0,0 +1,12 @@
+def binary_search(li, item):
+    """If item is in li, return the index.  Otherwise return -1.
+
+    Parameters:
+    - col is a sorted li.
+    - item is a sought-for item.
+
+    Each item in the list must be comparable with the sought-for item using
+    operator<.
+    
+    """
+    pass
---
+++ Python/binary_search_test.py
@@ -0,0 +1,6 @@
+from binary_search import binary_search as bsr
+import nose
+from nose.tools import eq_
+
+
+
